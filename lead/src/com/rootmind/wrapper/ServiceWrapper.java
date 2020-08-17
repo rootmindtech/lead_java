0f4e67d8824d5ea99a7912d9cc2e5ce48e5caf67
saikirangandham <rootmindtech@gmail.com>
2020-08-17 07:04:17 +0400

    First time commit of Lead java

---
+++ lead/src/com/rootmind/wrapper/ServiceWrapper.java
@@ -0,0 +1,18 @@
+package com.rootmind.wrapper;
+
+import java.math.BigDecimal;
+
+public class ServiceWrapper extends AbstractWrapper {
+
+	public String riderRefNo = null;
+	public String riderID = null;
+	public String serviceCode = null;
+	public String status = null;
+
+	public String vehicleType = null;
+	public BigDecimal currentLat = null;
+	public BigDecimal currentLng = null;
+
+	public boolean recordFound = false;
+
+}
